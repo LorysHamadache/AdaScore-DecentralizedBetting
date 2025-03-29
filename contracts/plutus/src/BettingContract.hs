be5c13ba57ee763a2444f6976b80f709d0caec9c
LorysHamadache <lorys.hamadache@hotmail.com>
2025-03-29 21:35:34 +0100

    Adding Aiken

--- contracts/plutus/src/BettingContract.hs
+++ contracts/plutus/src/BettingContract.hs
@@ -28,14 +28,14 @@ import           Plutus.V1.Ledger.Interval
 mkValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
 mkValidator datumData redeemerData scontextData =
     if (
-    (length scriptinputs == 1) &&
-    (d_fee datum == (getFeeCalculation $ d_amount datum)) &&
-    (r_matchID redeemer == d_matchID datum) &&
-    (d_creatorbet datum /= Unknown) &&
-    ((d_odds datum)> 100 && (d_odds datum) <= 1000) &&
-    (d_amount datum >=  bet_minamount) &&
-    (d_resultlimAt datum  > d_closedAt datum) &&
-    (d_result datum == Unknown) &&
+    (length scriptinputs == 1) && -- Added
+    (d_fee datum == (getFeeCalculation $ d_amount datum)) && --Def not needed
+    (r_matchID redeemer == d_matchID datum) && -- Added
+    (d_creatorbet datum /= Unknown) && -- Not needed as incentive is to put the right element
+    ((d_odds datum)> 100 && (d_odds datum) <= 1000) && -- Added 
+    (d_amount datum >=  bet_minamount) && -- Added 
+    (d_resultlimAt datum  > d_closedAt datum) && -- Unsure if necessary. We are checking the datum by the better, but he has incentive to have the right limit date
+    (d_result datum == Unknown) && -- Doesnt matter, the result is taken from the oracle, it is just for reporting purposes at the end
     validate_action (r_action redeemer)
     )
     then ()
@@ -61,14 +61,14 @@ mkValidator datumData redeemerData scontextData =
 {-# INLINABLE mkValidatorAccept #-}
 mkValidatorAccept :: BetDatum -> BetRedeemer -> TxInfo -> TxOut -> Bool
 mkValidatorAccept datum redeemer info scinput= 
-    (AwaitingBet == d_status datum) &&
-    (r_creator redeemer == d_creator datum) && 
-    (after (d_closedAt datum) (txInfoValidRange info)) &&
-    ((Ada.getLovelace $ Ada.fromValue $ txOutValue scinput) == better_amount) &&
-    (getInputValue (txInfoInputs info) >= total_amount) &&
-    (length output == 1) &&
-    (output_datum{d_acceptor = d_acceptor datum} == datum{d_status = AwaitingResult}) &&
-    ((Ada.getLovelace $ Ada.fromValue $ txOutValue (head output))== total_amount)
+    (AwaitingBet == d_status datum) && -- OK
+    (r_creator redeemer == d_creator datum) && -- useless cause the d_creator is taken to make the transaction
+    (after (d_closedAt datum) (txInfoValidRange info)) && -- OK
+    ((Ada.getLovelace $ Ada.fromValue $ txOutValue scinput) == better_amount) && -- OK
+    (getInputValue (txInfoInputs info) >= total_amount) && -- OK
+    (length output == 1) && -- OK
+    (output_datum{d_acceptor = d_acceptor datum} == datum{d_status = AwaitingResult}) && --OK
+    ((Ada.getLovelace $ Ada.fromValue $ txOutValue (head output))== total_amount) -- OK
     where
         better_amount = d_fee datum + d_amount datum
         total_amount  = (PlutusTx.Prelude.divide ((d_amount datum) * (d_odds datum)) 100)+ (d_fee datum)
@@ -83,13 +83,13 @@ mkValidatorAccept datum redeemer info scinput=
 mkValidatorCancel :: BetDatum -> BetRedeemer -> TxInfo -> TxOut -> Bool
 mkValidatorCancel datum redeemer info scinput = 
     if (d_status datum == AwaitingBet) then
-        (better_amount == (Ada.getLovelace $ Ada.fromValue $ txOutValue scinput)) &&
-        (length (filter (\x ->  getPubKeyHash x == (getPubKeyHash $ unPaymentPubKeyHash $ (d_creator datum))) (txInfoSignatories info)) >= 1) && 
-        (getTxValueAt (d_creator datum) (txInfoOutputs info) == getTxValueAt (d_creator datum) (map txInInfoResolved  (txInfoInputs info)) + better_amount - cardano_fee)
+        (better_amount == (Ada.getLovelace $ Ada.fromValue $ txOutValue scinput)) && -- OK
+        (length (filter (\x ->  getPubKeyHash x == (getPubKeyHash $ unPaymentPubKeyHash $ (d_creator datum))) (txInfoSignatories info)) >= 1) && --OK
+        (getTxValueAt (d_creator datum) (txInfoOutputs info) == getTxValueAt (d_creator datum) (map txInInfoResolved  (txInfoInputs info)) + better_amount - cardano_fee) --OK
     else if (d_status datum == AwaitingResult) then
-        (total_amount == (Ada.getLovelace $ Ada.fromValue $ txOutValue scinput)) &&
-        ((length (filter (\x ->  getPubKeyHash x == (getPubKeyHash $ unPaymentPubKeyHash $ (d_creator datum))) (txInfoSignatories info)) >= 1) ||
-        (length (filter (\x ->  getPubKeyHash x == (getPubKeyHash $ unPaymentPubKeyHash $ (d_acceptor datum))) (txInfoSignatories info)) >= 1)) &&
+        (total_amount == (Ada.getLovelace $ Ada.fromValue $ txOutValue scinput)) && --OK
+        ((length (filter (\x ->  getPubKeyHash x == (getPubKeyHash $ unPaymentPubKeyHash $ (d_creator datum))) (txInfoSignatories info)) >= 1) || -- OK
+        (length (filter (\x ->  getPubKeyHash x == (getPubKeyHash $ unPaymentPubKeyHash $ (d_acceptor datum))) (txInfoSignatories info)) >= 1)) && --OK
         (creator_refund_diff >= 0 && acceptor_refund_diff >= 0 && creator_refund_diff <= cardano_fee && acceptor_refund_diff <= cardano_fee) &&
         (abs(creator_refund_diff - acceptor_refund_diff) == cardano_fee) &&
         (before (d_resultlimAt datum) (txInfoValidRange info))
@@ -105,14 +105,14 @@ mkValidatorCancel datum redeemer info scinput =
 {-# INLINABLE mkValidatorClose #-}
 mkValidatorClose :: BetDatum -> BetRedeemer -> TxInfo -> TxOut  -> Bool
 mkValidatorClose datum redeemer info scinput = 
-    (AwaitingResult == d_status datum) &&
-    (length ref_inputs_list == 1) && 
-    (txOutAddress ref_input == pubKeyHashAddress (oracle_pkh) Nothing) &&
-    (total_amount == (Ada.getLovelace $ Ada.fromValue $ txOutValue scinput)) &&
-    ((o_result oracle_datum /= Unknown) && (o_matchID oracle_datum == d_matchID datum)) &&
-    (before (d_closedAt datum) (txInfoValidRange info)) &&
-    ((after (d_resultlimAt datum) (txInfoValidRange info)) && (after (o_end oracle_datum) (txInfoValidRange info))) &&
-    (getTxValueAt (oracle_pkh) (txInfoOutputs info) == getTxValueAt (oracle_pkh) (map txInInfoResolved  (txInfoInputs info)) + (d_fee datum)) &&
+    (AwaitingResult == d_status datum) && -- OK
+    (length ref_inputs_list == 1) && -- OK
+    (txOutAddress ref_input == pubKeyHashAddress (oracle_pkh) Nothing) && --OK
+    (total_amount == (Ada.getLovelace $ Ada.fromValue $ txOutValue scinput)) && -- OK
+    ((o_result oracle_datum /= Unknown) && (o_matchID oracle_datum == d_matchID datum)) && -- OK
+    (before (d_closedAt datum) (txInfoValidRange info)) && --Inutile, pas la reference de dispo
+    ((after (d_resultlimAt datum) (txInfoValidRange info)) && (after (o_end oracle_datum) (txInfoValidRange info))) && --sensé etre egal OK
+    (getTxValueAt (oracle_pkh) (txInfoOutputs info) == getTxValueAt (oracle_pkh) (map txInInfoResolved  (txInfoInputs info)) + (d_fee datum)) && 
     (getTxValueAt winner_ppkh (txInfoOutputs info) == getTxValueAt winner_ppkh (map txInInfoResolved  (txInfoInputs info)) + total_amount - (d_fee datum) - cardano_fee)  &&
     (length (filter (\x ->  getPubKeyHash x == (getPubKeyHash $ unPaymentPubKeyHash $ winner_ppkh)) (txInfoSignatories info)) >= 1) 
     where
